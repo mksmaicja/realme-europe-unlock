9bf06c4933e4712f2777c1239a5df05602eb449c
maicja <118010017+mksmaicja@users.noreply.github.com>
2023-11-07 16:05:39 +0100

    Add files

---
+++ run.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+set -e
+
+dir=/data/local/tmp
+adb=${adb:-"adb"}
+
+$adb version
+echo
+echo 'Device info:'
+$adb shell 'echo ro.build.product = $(getprop ro.build.product)'
+$adb shell 'echo ro.product.name = $(getprop ro.product.name)'
+$adb shell 'echo ro.build.oplus_nv_id = $(getprop ro.build.oplus_nv_id)'
+$adb shell 'echo ro.build.display.full_id = $(getprop ro.build.display.full_id)'
+$adb shell 'echo ro.build.fingerprint = $(getprop ro.build.fingerprint)'
+echo
+
+$adb shell rm -rf ${dir}/dirtypipe-android ${dir}/env-patcher ${dir}/startup-root ${dir}/magisk
+$adb push dirtypipe-android env-patcher startup-root magisk/ ${dir}
+$adb shell chmod 755 ${dir}/dirtypipe-android ${dir}/env-patcher ${dir}/startup-root ${dir}/magisk/busybox ${dir}/magisk/magiskinit ${dir}/magisk/magisk
+echo
+$adb shell ${dir}/dirtypipe-android -f /vendor/lib/libcamxifestriping.so
+echo
+
+echo 'Waiting 30 seconds before checking if it worked...'
+sleep 30
+$adb shell 'echo ro.product.name = $(getprop ro.product.name)'
